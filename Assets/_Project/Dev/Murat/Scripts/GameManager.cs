1b3f9da7ad595f1ed2a8d5f3f4108f12745fc105
Daan <144342233+Bananagames24@users.noreply.github.com>
2025-06-17 10:23:18 +0200

    BetaBuild 1.0
    Terug naar Windows Platform geswitched

--- Assets/_Project/Dev/Murat/Scripts/GameManager.cs
+++ Assets/_Project/Dev/Murat/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -126,6 +127,7 @@ public class GameManager : MonoBehaviour
 
     public void RevealScore()
     {
+        endResultScreen.SetActive(true);
         if (score1 > score2)
         {
             winLose[0].text = "Win";
@@ -142,15 +144,17 @@ public class GameManager : MonoBehaviour
             winLose[1].text = "Tie";
         }
         
-        for (int i = 0; i < players.Count; i++)
-        {
-            endResultScreen.SetActive(false);
-            players[i].score = 0;
-        }
+        
     }
+    
     public void HideScore()
     {
+        for (int i = 0; i < players.Count; i++)
+        {
+            players[i].score = 0;
+        }
         endResultScreen.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void IncreaseScore(int amount, bool player1)
