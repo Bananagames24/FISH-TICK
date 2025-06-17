83c397a9ca9d13a86c9b2eba36330407c90235f2
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2025-06-17 09:06:37 +0200

    electric eel

--- Assets/_Project/Dev/Murat/Scripts/Eel.cs
+++ Assets/_Project/Dev/Murat/Scripts/Eel.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Eel : MonoBehaviour
 {
     private GameManager gameManeger;
     private bool abilityActive = false;
+    private bool abilityDisabled = false;
     private int playerInput;
+    private NavMeshAgent navMeshAgent;
+    float yPos = 0.5f;
+    float xPos;
+    float zPos;
+    Vector3 Destination;
 
     void Start()
     {
         gameManeger = FindAnyObjectByType<GameManager>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(EelMovement());
     }
 
     private void Update()
@@ -32,27 +41,57 @@ public class Eel : MonoBehaviour
         {
             if (playerInput == 0)
             {
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, 3), Time.deltaTime);
+                navMeshAgent.SetDestination(new Vector3(transform.position.x, transform.position.y, 3));
                 if (transform.position.z > 2)
                 {
                     StartCoroutine(gameManeger.EelAbillity(playerInput));
                     abilityActive = false;
+                    abilityDisabled = true;
                     Destroy(gameObject,3);
                     Debug.Log(playerInput);
                 }
             }
             else if (playerInput == 1)
             {
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, -3), Time.deltaTime);
+                navMeshAgent.SetDestination(new Vector3(transform.position.x, transform.position.y, -3));
                 if (transform.position.z < -2)
                 {
                     StartCoroutine(gameManeger.EelAbillity(playerInput));
                     abilityActive = false;
+                    abilityDisabled = true;
                     Destroy(gameObject, 3);
                     Debug.Log(playerInput);
                 }
             }
+        }else
+        {
+            if(abilityDisabled)
+            {
+
+            }else
+            {
+                
+                float dist = Vector3.Distance(transform.position, Destination);
+                if (dist < 1f)
+                {
+                    navMeshAgent.SetDestination(EelMovement());
+                    Debug.Log("dest");
+                }
+            }
         }
+    }
+    private Vector3 EelMovement()
+    {
+        xPos = Random.Range(-3f, 3f);
+        zPos = Random.Range(-9.5f, -1);
+        if(playerInput == 0)
+        {
+            Destination = new Vector3(xPos, yPos, zPos);
+        }else if(playerInput == 1)
+        {
+            Destination = new Vector3(xPos, yPos, -zPos);
+        }
+        return Destination;
     }
     private void EelInAction()
     {
@@ -62,6 +101,7 @@ public class Eel : MonoBehaviour
         if (transform.position.z<0)
         {
             playerInput = 0;
+
         }
         else
         {
